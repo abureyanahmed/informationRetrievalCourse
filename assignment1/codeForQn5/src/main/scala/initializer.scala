4d3adf3248cf95156d98073ad5c16519afbbd8b0
mithun <mithunpaul08@gmail.com>
2017-01-29 02:10:14 -0700

    Make your parser recognize a word after (

--- assignment1/codeForQn5/src/main/scala/initializer.scala
+++ assignment1/codeForQn5/src/main/scala/initializer.scala
@@ -118,9 +118,9 @@ object initializer extends SimpleParser {
           println("Eg: (drug OR treatment) AND schizophrenia")
           println("Type your input here:")
           val queryStringForMl = StdIn.readLine()
-          parse(parenthesis, queryStringForMl) match {
+          parse(parWord, queryStringForMl) match {
             case Success(matched, _) => println("Found what you asked me to find which is:"+matched)
-            case Failure(msg, _) => println("FAILURE: " + msg)
+            case Failure(msg, _) => println("There is something wrong with the query you entered.: " + msg)
             case Error(msg, _) => println("ERROR: " + msg)
 
           }
--- assignment1/codeForQn5/src/main/scala/parsercombinators/SimpleParser.scala
+++ assignment1/codeForQn5/src/main/scala/parsercombinators/SimpleParser.scala
@@ -11,9 +11,19 @@ case class WordFreq(word: String, count: Int) {
     "occurs with frequency " + count
 }
 
+
+case class parsedQuery(word: String, operator: String) {
+  override def toString = "query term 1 is  <" + word + "> " +
+    "and the operator is" + operator
+}
+
 class SimpleParser extends RegexParsers {
   def parenthesis: Parser[String]   = """[(]+""".r       ^^ { _.toString }
   def word: Parser[String]   = """[a-z]+""".r       ^^ { _.toString }
+  def operator: Parser[String]   = """[AND]+""".r       ^^ { _.toString }
   def number: Parser[Int]    = """(0|[1-9]\d*)""".r ^^ { _.toInt }
+
   def freq: Parser[WordFreq] = word ~ number        ^^ { case wd ~ fr => WordFreq(wd,fr) }
+  def parWord: Parser[parsedQuery] = parenthesis ~ word         ^^ { case par ~ wd => parsedQuery(par,wd) }
+  //def firstQuery: Parser[parsedQuery] = parenthesis ~ word ~ word        ^^ { case wd ~ fr => parsedQuery(parenthesis,wd) }
 }
