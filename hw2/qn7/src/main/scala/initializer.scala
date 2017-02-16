1a2be2e4d2e19eb5872f6481cc20f87bc5c55ee8
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2017-02-16 15:21:25 -0700

    just changed some commands shown to user

--- hw2/qn7/src/main/scala/initializer.scala
+++ hw2/qn7/src/main/scala/initializer.scala
@@ -64,7 +64,7 @@ object initializer extends SimpleParser {
   //main used for including qn5.3
   def main(args: Array[String]): Unit = {
 
-    println("Hi Siri, welcome to world of Boolean Information Retrieval")
+    println("Hi Siri, welcome to world of Boolean Information Retrieval- HW2")
     println("We are first going to print the dictionary-postings inverted index for the given input file.")
     println()
     booleansearch.Utilities.readFromFile()
@@ -74,9 +74,10 @@ object initializer extends SimpleParser {
     breakable {
       while (true) {
         println("*************************************************")
+        println("What would you like to do?")
         println("Type 0 to exit.")
-        println("Type 1 for simple two word boolean queries .")
-        println("Type 2 for multiword boolean queries .")
+        println("Type 1 for Boolean proximity queries (non directional)")
+        println("Type 2 for Boolean proximity queries (directional)")
 
         println("Type your input here:")
         val typeOfProgram = StdIn.readLine()
@@ -91,8 +92,8 @@ object initializer extends SimpleParser {
         else if (typeOfProgram == "1") {
           println()
           println("What boolean two word simple query would you like to run on this index? Please capitalize the operator as shown in Examples below.")
-          println("Eg: schizophrenia AND drug")
-          println("Eg: breakthrough OR new")
+          println("Eg: schizophrenia /2 drug")
+          println("Eg: breakthrough /3 new")
           println("Type your input here:")
           queryString = StdIn.readLine()
           val verify = booleansearch.Utilities.verifyInputQueryStringForBinaryQuery(queryString)
