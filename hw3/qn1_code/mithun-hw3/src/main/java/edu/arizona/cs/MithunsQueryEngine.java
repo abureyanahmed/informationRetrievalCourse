84cc124370d5160f1cec7584e6bd7af5b0c9d98c
mithun <mithunpaul08@gmail.com>
2017-03-13 18:11:05 -0700

    got answers for qn 1.1 and 1.3

--- hw3/qn1_code/mithun-hw3/src/main/java/edu/arizona/cs/MithunsQueryEngine.java
+++ hw3/qn1_code/mithun-hw3/src/main/java/edu/arizona/cs/MithunsQueryEngine.java
@@ -26,7 +26,7 @@ public class MithunsQueryEngine {
     public static void main(String[] args) {
 
         try {
-            System.out.println("Welcome to  Homework 3!");
+            System.out.println("********Welcome to  Homework 3!");
 
             StandardAnalyzer analyzer = new StandardAnalyzer();
             Directory index = new RAMDirectory();
@@ -34,16 +34,48 @@ public class MithunsQueryEngine {
             IndexWriterConfig config = new IndexWriterConfig(analyzer);
 
             IndexWriter w = new IndexWriter(index, config);
-            addDoc(w, "information retrieval is the most awesome class I ever took.");
-            addDoc(w, "the retrieval of private information from your emails is a job that the NSA loves.");
-            addDoc(w, "at university of arizona you learn about data science.");
-            addDoc(w, "the labrador retriever is a great dog.");
+            addDoc(w, "information retrieval is the most awesome class I ever took.","Document #1");
+            addDoc(w, "the retrieval of private information from your emails is a job that the NSA loves.","Document #2");
+            addDoc(w, "at university of arizona you learn about data science.","Document #3");
+            addDoc(w, "the labrador retriever is a great dog.","Document #4");
             w.close();
 
-           // Query myQuery = readQuery(args, analyzer);
-           // Query myQuery = readQuery(args, analyzer);
 
-            String querystr = args.length > 0 ? args[0] : "lucene";
+
+
+            //String querystr = "title:information AND title:dog";
+
+            //args.length > 0 ? args[0] : "lucene";
+
+
+            //String querystr = "title:information OR title:retrieval";Score:1.1655893
+            String querystr = "information AND retrieval";
+
+            //Score:1.1655893
+            //String querystr = "title:information AND title:retrieval"; //Score:1.1655893
+            //String querystr = "title:awesome OR title:retrieval";
+            /*1. Document #1	Score:1.595089
+2. Document #2	Score:0.58279467
+*/
+
+            /*String querystr = "title:retrieval   -title:awesome";
+            For Qn 1.1 found 1 hits for your query:title:retrieval   -title:awesome
+1. Document #2	Score:0.58279467
+*/
+
+            //String querystr = "title:information -title:retrieval";
+            /*String querystr = "title:information AND -title:retrieval";
+            For Qn 1.1 found 0 hits for your query:title:information AND -title:retrieval
+                    */
+
+//            for(int i=0;i<args.length;i++)
+//            {
+//                System.out.println(args[i]);
+//                querystr=querystr+args[i];
+//                querystr=querystr+" ";
+//            }
+
+            System.out.println("the query string is:"+querystr);
             Query myQuery= new QueryParser("title", analyzer).parse(querystr);
 
 
@@ -51,7 +83,7 @@ public class MithunsQueryEngine {
             IndexSearcher searcher = new IndexSearcher(reader);
 
             ScoreDoc[] hits = searchEngine(searcher, myQuery);
-            displayResults(searcher, hits);
+            displayResults(searcher, hits,querystr);
         }
         catch (IOException ex)
         {
@@ -62,27 +94,12 @@ public class MithunsQueryEngine {
         }
     }
 
-    private static void addDoc(IndexWriter w, String title) throws IOException {
+    private static void addDoc(IndexWriter w, String title, String docId) throws IOException {
         Document doc = new Document();
         doc.add(new TextField("title", title, Field.Store.YES));
-        //doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        doc.add(new StringField("docid", docId, Field.Store.YES));
         w.addDocument(doc);
     }
-
-//    private static Query readQuery(String[] args, StandardAnalyzer analyzer) {
-//
-//        QueryParser qprsr=
-//        Query q= new Query();
-//        try {
-//
-//
-//       }
-//       catch (ParseException ex) {
-//           System.out.println(ex.getMessage());
-//       }
-//
-//        return q;
-//    }
 
     private static ScoreDoc[] searchEngine( IndexSearcher searcher, Query myQuery) {
         int hitsPerPage = 10;
@@ -98,14 +115,15 @@ public class MithunsQueryEngine {
         return hits;
     }
 
-    private static void displayResults( IndexSearcher searcher,ScoreDoc[] hits) {
+    private static void displayResults( IndexSearcher searcher,ScoreDoc[] hits, String querystr) {
 
         try {
-            System.out.println("Found " + hits.length + " hits.");
+            System.out.println("For Qn 1.1 found " + hits.length + " hits for your query:"+querystr);
             for (int i = 0; i < hits.length; ++i) {
                 int docId = hits[i].doc;
                 Document d = searcher.doc(docId);
-                System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+                float docScore = hits[i].score ;
+                System.out.println((i + 1) + ". " + d.get("docid") + "\t" +"Score:"+docScore);
             }
         }
         catch (IOException ex) {
---
+++ hw3/qn1_code/readme.txt
@@ -0,0 +1,13 @@
+use maven
+
+mvn compile
+mvn 
+
+
+todo: 
+
+update mvn version
+submit class path addition shell script?
+move to another folder and check
+or move to chung.cs. and check
+
