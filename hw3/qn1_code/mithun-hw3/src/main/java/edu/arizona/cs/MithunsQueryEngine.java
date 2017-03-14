b2da60fbbb2e5c84fc1f68a970d7da99c29988dc
mithun <mithunpaul08@gmail.com>
2017-03-14 02:41:24 -0700

    ready for submission

--- hw3/qn1_code/mithun-hw3/src/main/java/edu/arizona/cs/MithunsQueryEngine.java
+++ hw3/qn1_code/mithun-hw3/src/main/java/edu/arizona/cs/MithunsQueryEngine.java
@@ -2,7 +2,8 @@ package edu.arizona.cs;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import java.io.IOException;
-
+import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -18,6 +19,7 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
@@ -32,49 +34,29 @@ public class MithunsQueryEngine {
             Directory index = new RAMDirectory();
 
             IndexWriterConfig config = new IndexWriterConfig(analyzer);
+           config.setSimilarity(new ClassicSimilarity());
 
             IndexWriter w = new IndexWriter(index, config);
-            addDoc(w, "information retrieval is the most awesome class I ever took.","Document #1");
+
+
+            addDoc(w, "information retrieval is the most awesome information class I ever took.","Document #1");
             addDoc(w, "the retrieval of private information from your emails is a job that the NSA loves.","Document #2");
-            addDoc(w, "at university of arizona you learn about data science.","Document #3");
+            addDoc(w, "at university of arizona you learn information about data science.","Document #3");
             addDoc(w, "the labrador retriever is a great dog.","Document #4");
             w.close();
 
 
 
 
-            //String querystr = "title:information AND title:dog";
 
-            //args.length > 0 ? args[0] : "lucene";
-
-
-            //String querystr = "title:information OR title:retrieval";Score:1.1655893
-            //String querystr = "information AND retrieval";
-
-            //Score:1.1655893
-            //String querystr = "title:information AND title:retrieval"; //Score:1.1655893
-            //String querystr = "title:awesome OR title:retrieval";
-            /*1. Document #1	Score:1.595089
-2. Document #2	Score:0.58279467
-*/
-
-            /*String querystr = "title:retrieval   -title:awesome";
-            For Qn 1.1 found 1 hits for your query:title:retrieval   -title:awesome
-1. Document #2	Score:0.58279467
-*/
-
-            //String querystr = "title:information -title:retrieval";
-            /*String querystr = "title:information AND -title:retrieval";
-            For Qn 1.1 found 0 hits for your query:title:information AND -title:retrieval
-                    */
             String querystr="";
-            //for proximity queries we wanted the syntax with double quotes
-            //Eg:"retriever dog"~3
 
 
             //if the length of arguments is less than or equal to 3, we assume simple boolean query.
 
             if(args.length>3) {
+                //for proximity queries we wanted the syntax with double quotes
+                //Eg:"retriever dog"~3
                 querystr = querystr+"\"";
                 //else assume proximity query
                 for (int i = 0; i < args.length; i++) {
@@ -143,12 +125,17 @@ public class MithunsQueryEngine {
         ScoreDoc[] hits= new ScoreDoc[99999];
 
         try {
+
+            searcher.setSimilarity(new ClassicSimilarity());
             TopDocs docs = searcher.search(myQuery, hitsPerPage);
             hits = docs.scoreDocs;
+            //System.out.println(searcher.explain(myQuery, hits[0].doc));
 
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+
+
         return hits;
     }
 
--- hw3/qn1_code/readme.txt
+++ hw3/qn1_code/readme.txt
@@ -1,20 +1,40 @@
-use maven
+The versions of various tools used in this code are as follows:
 
-mvn compile
-mvn 
+Tool: Java
+java version "1.8.0_77"
+Java(TM) SE Runtime Environment (build 1.8.0_77-b03)
+Java HotSpot(TM) 64-Bit Server VM (build 25.77-b03, mixed mode)
 
+Tool: Lucene
+<version>6.4.2</version>
 
-todo: 
-
-update mvn version
-submit class path addition shell script?
-move to another folder and check
-or move to chung.cs. and check
-
-
-For Qn 1.3: Please use the following entries in pom.xml to test the 3 sections of Qn 1.3
+Tool: Maven
+Apache Maven 3.3.9 (bb52d8502b132ec0a5a3f4c09453c07478323dc5; 2015-11-10T09:41:47-07:00)
 
 
+Steps to Export Path:
+
+Add the following to ~/.profile and ~/.bash_profile
+export JAVA_HOME= `/usr/libexec/java_home -v 1.8`
+export PATH=/Users/mithun/Desktop/apache-maven-3.3.9/bin:$PATH
+
+Note: this will change depending on the location of java and maven in your machine.
+
+
+Steps to Compile/Run:
+
+1. change directory to the directory which has pom.xml
+Eg: cd /hw3/qn1_code/mithun-hw3/
+2. mvn compile
+3. mvn exec:java
+
+
+Notes:
+1.For Qn 1.1: Please use the following entry in pom.xml to test 
+ 	 <!--For Qn 1.1-->
+        <argument>information</argument>
+        <argument>retrieval</argument>
+2. For Qn 1.3: Please use the following entries in pom.xml to test the 3 sections of Qn 1.3
  	<!--For Qn 1.3.a-->
         <argument>information</argument>
         <argument>AND</argument>
@@ -32,4 +52,5 @@ For Qn 1.3: Please use the following entries in pom.xml to test the 3 sections o
         <argument>1</argument>
 
 Note: If you test more boolean queries, make sure they are all in the same argument. If there are more than 3 arguments, my code assumes that the 4th argument is the proximity distance.
+
 
