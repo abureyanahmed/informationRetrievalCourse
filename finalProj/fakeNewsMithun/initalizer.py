661913693570568c5f58dd7149b9aa9c6f512e79
mithun <mithunpaul08@gmail.com>
2017-04-30 21:36:27 -0700

    added code to calculate tf-idf

--- finalProj/fakeNewsMithun/initalizer.py
+++ finalProj/fakeNewsMithun/initalizer.py
@@ -19,9 +19,19 @@ from csv import DictReader
 #this is just the first file which has main function and strings together various sub modules.
 
 if __name__ == "__main__":
+    #make sure that the current working directory is the starting level
+    # print("value of self.path is :"+ self.path)
+    cwd = os.getcwd()
+   # print("current directory is:" + cwd)
+    base_dir_name = os.path.dirname(os.path.abspath(sys.argv[0]))
+   # print("base directory is:" + base_dir_name)
+    if(base_dir_name != cwd):
+        os.chdir(base_dir_name)
+
 
     #read the data first
-    d = utils.dataset.DataSet()
+    cwd = os.getcwd()
+    d = utils.dataset.DataSet(cwd)
 
     # Create a term document index
     counts = [[3, 0, 1],[2, 0, 0],[3, 0, 0],[4, 0, 0],[3, 2, 0],[3, 0, 2]]
@@ -29,7 +39,7 @@ if __name__ == "__main__":
     #give the counts/frequencies of terms to calculate tf-idf of the document
     objdp= src.dataProcessing.ProcessData()
     my_tf_idf_score=objdp.calculate_tf_idf(counts)
-    print my_tf_idf_score
+    print(my_tf_idf_score)
 
 
 
--- finalProj/fakeNewsMithun/utils/dataset.py
+++ finalProj/fakeNewsMithun/utils/dataset.py
@@ -1,13 +1,17 @@
 from csv import DictReader
-
+import os
+import sys
 
 class DataSet():
-    def __init__(self, path="fnc-1"):
-        self.path = path
+    def __init__(self, cwd):
 
+        #cwd = os.getcwd()
+        #print("inside DataSet, current directory is:" + cwd)
+        self.path = cwd+"/src/"
+        #print("inside DataSet, current path is:" + self.path)
         print("Reading dataset")
-        bodies = "train_bodies.csv"
-        stances = "train_stances.csv"
+        bodies = "train_bodies_original.csv"
+        stances = "test_stances_csc483583.csv"
 
         self.stances = self.read(stances)
         articles = self.read(bodies)
@@ -21,7 +25,8 @@ class DataSet():
         for article in articles:
             self.articles[int(article['Body ID'])] = article['articleBody']
 
-        print("Total stances: " + str(len(self.stances)))
+        print("Number of Total stances: " + str(len(self.stances)))
+        print("and the stances are : " + self.stances)
         print("Total bodies: " + str(len(self.articles)))
 
 
@@ -30,7 +35,6 @@ class DataSet():
         rows = []
         with open(self.path + "/" + filename, "r", encoding='utf-8') as table:
             r = DictReader(table)
-
             for line in r:
                 rows.append(line)
         return rows
